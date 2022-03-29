d16afc36ddcb1ea3b37f9d968fc4548e151ec606
simonek48 <73053167+simonek48@users.noreply.github.com>
2022-03-29 10:59:36 +0200

    Add files via upload

---
+++ loginandregister.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE IF NOT EXISTS `loginandregister` ;
+USE `loginandregister`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` int(11) DEFAULT NULL,
+  `email` int(11) DEFAULT NULL,
+  `password` int(11) DEFAULT NULL
+);
---
+++ registered.php
@@ -0,0 +1,43 @@
+<?php
+  include 'connect.php';
+  $user_id = "SELECT COUNT(*) FROM users;";
+  $count = mysqli_query($conn, $user_id);
+  $row = mysqli_fetch_assoc($count);
+  $uid = implode($row) + 1;
+
+ 
+  $username = $_POST['username'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  
+  $s = " SELECT * FROM users where username = '$username'";
+  $result = mysqli_query($conn, $s);
+  $num = mysqli_num_rows($result);
+
+  if($num == 1){
+    echo "Username already taken";
+  }
+  else{ 
+    $insert = "INSERT INTO users(id, username, email, password)
+    values('$uid', '$username', '$email', '$password')";
+    $result= mysqli_query($conn, $insert); ?>  
+    <!DOCTYPE html>
+
+<html>
+  <head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <meta charset="utf-8">
+    <title>register_success</title>
+  </head>
+  <body>
+    <h1>Registered succesfully</h1>
+    <form action = "main.php" method = "POST">
+    <button type = "submit" class = "btn btn-primary"> Go to main page </button>
+  </body>
+
+</html>
+<?php }
+ ?>    
+
