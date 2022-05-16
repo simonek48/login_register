759c7cccb41550e2715343d721e505063b4ee274
simonek48 <73053167+simonek48@users.noreply.github.com>
2022-05-16 17:57:33 +0200

    Add files via upload

--- deleteuser.php
+++ deleteuser.php
@@ -10,6 +10,12 @@ while($row = mysqli_fetch_array($query1))
 }
 if ($conn->query($sql) === TRUE){
     echo "DELETED SUCCESSFULLY";
+?>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <form action = "list.php" method = "POST">
+    <button type = "submit" class = "btn btn-outline-dark"> Go to main page </button>
+<?php
 }
 
 else{
