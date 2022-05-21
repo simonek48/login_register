a66c8c8516eba60cec31241fe6482c2336120b3d
simonek48 <73053167+simonek48@users.noreply.github.com>
2022-05-21 19:08:03 +0200

    Add files via upload

--- loginandregister.sql
+++ loginandregister.sql
@@ -1,9 +1,15 @@
-CREATE DATABASE IF NOT EXISTS `loginandregister` ;
+CREATE DATABASE IF NOT EXISTS `loginandregister` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
 USE `loginandregister`;
 
+CREATE TABLE IF NOT EXISTS `profileimg` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL DEFAULT '',
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) 
+
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) DEFAULT NULL,
-  `username` VARCHAR(11) DEFAULT NULL,
-  `email` VARCHAR(11) DEFAULT NULL,
-  `password` VARCHAR(11) DEFAULT NULL
-);
+  `username` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(50) DEFAULT NULL
+)
